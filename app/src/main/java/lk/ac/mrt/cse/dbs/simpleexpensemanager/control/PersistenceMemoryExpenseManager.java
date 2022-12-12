332e69ac076cd88bdb2953751859a34e6d7a3e7c
gimhansandeeptha <118417911+gimhansandeeptha@users.noreply.github.com>
2022-12-13 04:59:52 +0530

    Update PersistenceMemoryExpenseManager.java

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistenceMemoryExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistenceMemoryExpenseManager.java
@@ -29,10 +29,5 @@ public class PersistenceMemoryExpenseManager extends ExpenseManager{
         setAccountsDAO(persistenceMemoryAccountDAO);
         databaseHelper.close();
 
-//        // dummy data
-//        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
-//        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-//        getAccountsDAO().addAccount(dummyAcct1);
-//        getAccountsDAO().addAccount(dummyAcct2);
     }
 }
